99c954cab5dc09cbc09263a7fc325cb530fcfd2a
Tofazzol-Hossain <tofazzzol.hosssain@gmail.com>
2022-02-21 23:38:07 +0600

    everything done

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
+// Getting value from input field 
 function getValue(id) {
     const inputValue = parseFloat(document.getElementById(id).value);
-    if (isNaN(inputValue) || inputValue < 0 || inputValue == '') {
+    if (isNaN(inputValue) || inputValue < 0) {
         document.getElementById(id + '-error').style.display = 'block';
 
     } else {
@@ -9,7 +10,7 @@ function getValue(id) {
     }
 
 }
-
+// Adding all Expenses
 function getAllExpanse() {
     return (
         getValue('food-input') +
@@ -17,18 +18,19 @@ function getAllExpanse() {
         getValue('clothes-input')
     );
 }
-
+// setting input field blank 
 function doEmptyInput(id) {
     document.getElementById(id).value = '';
 }
 
+// calculate section 
 document
     .getElementById('calculate-button')
     .addEventListener('click', function () {
-        document.getElementById('calculate-error').style.display = 'none';
+        document.getElementById('income-error').style.display = 'none';
         const totalIncome = getValue('total-income');
         const totalExpanse = getAllExpanse();
-
+// error handaling 
         if (isNaN(totalIncome) || totalIncome < totalExpanse) {
             document.getElementById('income-error').style.display = 'block';
         }
@@ -47,18 +49,32 @@ document
         }
     });
 
+// Seving section 
 document.getElementById('saving-btn').addEventListener('click', function () {
     const totalIncome = getValue('total-income');
     const savingPercentage = getValue('saving-percentage');
     const savingAmount = (totalIncome / 100) * savingPercentage;
-
-    document.getElementById('saving-amount').innerText = savingAmount;
-
     const balance = parseFloat(document.getElementById('balance').innerText);
+    const remainingBalance = balance - savingAmount;
+    console.log(remainingBalance)
 
-    document.getElementById('remaining-balance').innerText =
-        balance - savingAmount;
 
-    doEmptyInput('saving-percentage');
-    doEmptyInput('total-income');
+
+// error handaling
+    if (isNaN(savingPercentage)) {
+        document.getElementById('saving-percentage-error').style.display = 'block';
+    }
+    else if (savingAmount > balance) {
+        document.getElementById('saving-error').style.display = 'block';
+        document.getElementById(saving - percentage - error).style.display = 'none';
+    }
+    else {
+        document.getElementById('saving-amount').innerText = savingAmount;
+        document.getElementById('remaining-balance').innerText = remainingBalance;
+
+
+// setting blank
+        doEmptyInput('saving-percentage');
+        doEmptyInput('total-income');
+    }
 });
